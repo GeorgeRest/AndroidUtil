70f73744169237621a202139f39e36d7308b528a
Georege <1561903398@qq.com>
2024-04-17 18:45:36 +0800

    Initial commit

---
+++ utilcodex/src/main/java/com/utils/utilcodex/subutil/util/RetrofitUtils.java
@@ -0,0 +1,13 @@
+package com.utils.utilcodex.subutil.util;
+
+/**
+ * <pre>
+ *     author: Blankj
+ *     blog  : http://blankj.com
+ *     time  : 2018/08/25
+ *     desc  : utils about retrofit
+ * </pre>
+ */
+public final class RetrofitUtils {
+
+}
---
+++ utilcodex/src/main/java/com/utils/utilcodex/subutil/util/http/Chain.java
@@ -0,0 +1,4 @@
+package com.utils.utilcodex.subutil.util.http;
+
+public interface Chain {
+}
